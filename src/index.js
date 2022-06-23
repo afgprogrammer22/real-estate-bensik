4f0b6ab79e70d041f74e7c7532fd208e892988dd
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-06-23 10:57:53 +0430

    General styles added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App';
-
-const root = ReactDOM.render(document.getElementById('root'))
+import './index.scss'
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
