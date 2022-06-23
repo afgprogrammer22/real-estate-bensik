fd03b6188f857404f35292877db76fa57a717b61
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-06-23 12:04:34 +0430

    Header section created and only navbar added

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.scss'
+import Navbar from '../../components/navbar/Navbar'
+const Header = () => {
+  return (
+    <div className='header'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Header
