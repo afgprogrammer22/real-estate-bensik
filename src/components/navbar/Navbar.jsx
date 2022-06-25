3443b22511f94bb479e726bef0987d16895e4a59
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-06-25 11:53:47 +0430

    Navbar with responsivity completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,65 @@
+import { useState } from 'react';
 import './navbar.scss';
-import logo from '../../assets/Logo.svg';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/Logo.svg';
+import { MdMenu, MdClose } from 'react-icons/md';
 const Navbar = () => {
+  // Toggle menu
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  // Navbar links
+  const links = [
+    { title: 'Home', to: '#' },
+    { title: 'About', to: '#' },
+    { title: 'Services', to: '#' },
+    { title: 'Listed', to: '#' },
+    { title: 'Contact', to: '#' },
+  ];
   return (
-    <div className='navbar'>
-      <img src={logo} alt='Bensik.' className='navbar__logo' />
-      <ul className='navbar__links'>
-        <li className='navbar__links-item'>
-          <Link to='#'>Home</Link>
-        </li>
-        <li className='navbar__links-item'>
-          <Link to='#'>Features</Link>
-        </li>
-        <li className='navbar__links-item'>
-          <Link to='#'>Service</Link>
-        </li>
-        <li className='navbar__links-item'>
-          <Link to='#'>Listed</Link>
-        </li>
-        <li className='navbar__links-item'>
-          <Link to='#'>Contact</Link>
-        </li>
-      </ul>
-      <div className='navbar__buttons'>
-        <button className='navbar__buttons-login'>Sign In</button>
-        <button className='navbar__buttons-register'>Register</button>
+    <nav className='navbar'>
+      {/* Logo */}
+      <Link to='/' className='logo'>
+        <img src={logo} alt='Bensik' />
+      </Link>
+
+      {/* Navbar Items */}
+      <div className='navbar__items'>
+        <ul className='navbar__items-links'>
+          {links.map((item, index) => (
+            <li key={index}>
+              <Link to={item.to}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className='navbar__items-buttons'>
+          <Link to='#' className='login-btn'>
+            Sign In
+          </Link>
+          <Link to='#' className='register-btn'>
+            Register
+          </Link>
+        </div>
       </div>
-    </div>
+      {/* Mobile Navbar Items */}
+
+      <div className='navbar__mobile'>
+        <div className='navbar__mobile-menu'>
+          <MdMenu onClick={() => setToggleMenu(true)} />
+        </div>
+        {toggleMenu && (
+          <div className='mobile__links'>
+            <MdClose onClick={() => setToggleMenu(false)} />
+            <ul>
+              {links.map((item, index) => (
+                <li key={index}>
+                  <Link to={item.to}>{item.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
   );
 };
 
