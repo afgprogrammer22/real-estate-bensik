293413069f1de23aec3a8156ff6f1ddc9ece6c02
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-06-26 11:23:30 +0430

    Header section completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,28 @@
-import './header.scss'
-import Navbar from '../../components/navbar/Navbar'
+import './header.scss';
+import Navbar from '../../components/navbar/Navbar';
+import hero from '../../assets/hero.png';
 const Header = () => {
   return (
     <div className='header'>
       <Navbar />
+      <div className='hero'>
+        <div className='hero__text'>
+          <p className='intro'>Welcome to Besnik Agency</p>
+          <h1>
+            Discover a place <br></br> you'll love to live.
+          </h1>
+          <p className='desc'>
+            Get the best real estate deals first, before they hit the mass
+            market! Hot foreclosure deals with one simple search
+          </p>
+          <button>More About Us</button>
+        </div>
+        <div className='hero__image'>
+          <img src={hero} alt='' />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
