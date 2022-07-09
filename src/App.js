737adf821e335542ec73844836968872458bb45e
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-07-09 10:36:18 +0430

    Newsletter part completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Routes } from 'react-router-dom';
 import Works from './containers/how_it_works/Works';
 import Featured from './containers/featured_properties/Featured';
 import Featured2 from './containers/featured_second/Featured2';
+import NewsLetter from './containers/newsletter/NewsLetter';
 const App = () => {
   return (
     <div>
@@ -14,6 +15,7 @@ const App = () => {
         <Works />
         <Featured />
         <Featured2 />
+        <NewsLetter />
       </Routes>
     </div>
   );
---
+++ src/containers/newsletter/NewsLetter.jsx
@@ -0,0 +1,15 @@
+import './newsletter.scss';
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+      <div className='desc'>
+        <h2>Featured Properties</h2>
+        <p>Everything you need to know when you're looking</p>
+        <button>Get Started Now</button>
+      </div>
+      <div className='mobile'></div>
+    </div>
+  );
+};
+
+export default NewsLetter;
